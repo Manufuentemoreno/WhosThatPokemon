175dca5cba5b7886762710e862fa8a701cc253ab
Manuel Fuente Moreno <manu-u@live.com.ar>
2022-10-22 11:58:41 -0300

    updating .gitignore to ignore the test.js file

--- src/test.js
+++
@@ -1,4 +0,0 @@
-// https://bulbapedia.bulbagarden.net/wiki/Generation#:~:text=To%20date%2C%20there%20are%20eight,own%20quirks%20into%20the%20franchise.
-
-// la opcion correcta nunca es la opción n°1
-// sigue con posibilidad de repetir opciones y crashear
